106923b2b60f68856ab4a02ed51386827e670a38
Ashika J <106242789+Ashikajayakumar@users.noreply.github.com>
2025-12-24 19:36:06 +0530

    Update 2_ Basic Calculator.java

--- Stack/4_MATH+STACK/2_ Basic Calculator.java
+++ Stack/4_MATH+STACK/2_ Basic Calculator.java
@@ -1,9 +1,82 @@
-                                          Basic Calculator
+
+                                    Basic Calculator
+
+ s consists of digits, '+', '-', '(', ')', and ' '.
+ s represents a valid expression.
+ '+' is not used as a unary operation (i.e., "+1" and "+(2 + 3)" is invalid).
+ '-' could be used as a unary operation (i.e., "-1" and "-(2 + 3)" is valid).
+
+
+  Initial:
+  
+        int result = 0;
+        int number = 0;
+        int sign = 1;
 
 
 
-import java.util.Stack;
+Example : 1+2-(3-78)
 
+
+  
+  
+Iteration : 1  "1"
+    number =1
+
+  
+Iteration : 2   "+"
+     result = result+(sign*number)     # result= 0+(1*1)=1
+     number = 0                         # number =0
+     sign = 1                           # sign=1
+
+  
+Iteration : 3   "2"
+  number=2
+
+  
+Iteration : 4   "-"
+     result = result+(sign*number)     # result= 1+(1*2)=3
+     number = 0                        # number =0
+     sign = -1                         # sign= -1
+
+  
+Iteration : 5  "("
+    stack.push(result);              # st=[3]
+    stack.push(sign);                # st=[3,-1]
+    result = 0;
+    sign = 1;
+
+
+Iteration : 6     "3" 
+   number=3
+
+  
+Iteration : 7     "-"
+    result = result+(sign*number)     # result= 0+(1*3)=3
+    number = 0                        # number =0
+    sign = -1                         # sign= -1
+
+  
+Iteration : 8    "78"
+    number = 78
+
+  
+Iteration : 9    ")"
+
+        result += sign * number;       #result= 3+(-1*78) = 3-78  =-75
+        number = 0;                    #number= 0
+        result *= stack.pop();         # result= -75 * -1 =75
+        result += stack.pop();         # result= 75 +3 =78
+
+  
+Last Iteration:
+  return result + sign * number;       # 75 + -1 * 0 = 75+0 =75
+
+
+
+  
+Implementation of Java Code
+  
 class Solution {
     public int calculate(String s) {
         Stack<Integer> stack = new Stack<>();
