afb2bfca447b36a99d85c4780d0bb3cb6c14b246
Ashika J <106242789+Ashikajayakumar@users.noreply.github.com>
2025-12-28 21:23:20 +0530

    Update 3_123 Pattern.java

--- Stack/6_ADVANCED INTERVIEW /3_123 Pattern.java
+++ Stack/6_ADVANCED INTERVIEW /3_123 Pattern.java
@@ -10,19 +10,20 @@ Iteration : 3                         nums[i]= ' 2 '
     if (nums[i] < third) → false
     while stack empty → skip                                                           #st= [2]
     st.push(2)
-
+--------------------------------------------------------------------------------------
 
   Iteration : 2                       nums[i]= ' 4 '
 
      if (4 < third) → false
      while (stack.peek() < 4)                                                          #st.peek()=2    st.peek()< 4   2<4
         third = 2 (pop)                                                                # third = 4
+  --------------------------------------------------------------------------------------
 
   Iteration : 1                       nums[i]= ' 1 '
 
      if(nums[i] < third) → true                                                        # nums[i] <third    1 < 4 
         retrun true;
-
+--------------------------------------------------------------------------------------
 
 
       nums[i] = 1
